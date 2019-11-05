42ec9a6e2579984d87a47a36a509e3eb8d490f54
Jason Johnson <jvsonjohnson@gmail.com>
2019-11-04 19:03:08 -0500

    Added Search Button

---
+++ app.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  var http = new this.XMLHttpRequest();
+
+  http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+
+  http.open("GET", "superheroes.php", true);
+  http.send();
+
+  var btn = document.getElementsByTagName("button");
+
+  btn[0].addEventListener("click", function() {
+    alert();
+  });
+};
